cfda260c306a605313a57118217e912976199a15
Renato <renato.melo@ab-inbev.com>
2023-02-21 16:38:03 -0300

    build: create automation to upload new images automatically

--- upload.js
+++ upload.js
@@ -1,5 +1,8 @@
 import { v2 as cloudinary } from 'cloudinary'
 import dotenv from 'dotenv'
+import fs from 'fs/promises'
+import path from "path"
+import { fileURLToPath } from "url"
 
 dotenv.config()
 cloudinary.config({
@@ -9,16 +12,45 @@ cloudinary.config({
   secure: true
 })
 
-async function uploadAssets() {
-  try {
-    const result = await cloudinary.uploader.upload('public/react.svg', {
-      use_filename: true,
-      unique_filename: false,
-      overwrite: true,
-      folder: 'cupcake'
-    })
+const __filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(__filename)
 
-    console.log({ result })
+async function uploadAssets() {
+  const images = await fs.readdir('./public/images')
+  const bufferState = await fs.readFile(path.join(dirname, 'public', 'images_changes.json'))
+  const currentState = bufferState.toString('utf8')
+  const newState = JSON.stringify(images)
+
+  if (currentState === newState) {
+    console.info('There is no new image to upload.')
+    return false
+  }
+
+  try {
+    await fs.writeFile('public/images_changes.json', JSON.stringify(images))
+  } catch (error) {
+    console.error(error.message)
+  }
+
+  const parsedCurrentState = JSON.parse(currentState)
+  const parsedNewState = JSON.parse(newState)
+  const newImages = parsedNewState.filter(image => {
+    return !parsedCurrentState.includes(image)
+  })
+
+  try {
+    const result = await Promise.all(newImages.map(async image => {
+      const filePath = path.join(dirname, 'public/images', image)
+
+      return await cloudinary.uploader.upload(filePath, {
+        use_filename: true,
+        unique_filename: false,
+        overwrite: true,
+        folder: 'cupcake',
+      })
+    }))
+
+    console.log('Files have been uploaded successfully.', result)
     return false
   } catch (error) {
     console.log({ error })
