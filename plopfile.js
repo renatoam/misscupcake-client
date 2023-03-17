69b8a4d6f427bbe6bfb521ee086d44e739055262
Renato <renato.melo@ab-inbev.com>
2023-03-16 23:06:32 -0300

    chore: config plop to generate boilerplate

---
+++ plopfile.js
@@ -0,0 +1,45 @@
+export default function (plop) {
+  plop.setGenerator('atom', {
+      description: '[Atom] Create Atomic Component',
+      prompts: [
+        {
+          type: 'input',
+          name: 'name',
+          message: 'atom name'
+        },
+        {
+          type: 'input',
+          name: 'wrapper',
+          message: 'HTML element to be the wrapper element',
+          default: 'section'
+        },
+      ],
+      actions: [
+        {
+          type: 'add',
+          path: 'src/components/atoms/{{name}}/index.ts',
+          templateFile: 'templates/index.hbs'
+        },
+        {
+          type: 'add',
+          path: 'src/components/atoms/{{name}}/{{name}}.tsx',
+          templateFile: 'templates/component.hbs'
+        },
+        {
+          type: 'add',
+          path: 'src/components/atoms/{{name}}/{{name}}.module.scss',
+          templateFile: 'templates/styles.hbs'
+        },
+        {
+          type: 'add',
+          path: 'src/components/atoms/{{name}}/{{name}}Props.ts',
+          templateFile: 'templates/props.hbs'
+        },
+        {
+          type: 'add',
+          path: 'src/components/atoms/{{name}}/{{name}}.test.ts',
+          templateFile: 'templates/tests.hbs'
+        },
+      ]
+  });
+};
