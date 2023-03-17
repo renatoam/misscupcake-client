160620c1acdf67a36d267b16f10f0da6c6c33a56
Renato <renato.melo@ab-inbev.com>
2023-03-16 23:05:58 -0300

    chore: config path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: false
+  publicDir: false,
+  build: {
+    sourcemap: true,
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+  }
 })
