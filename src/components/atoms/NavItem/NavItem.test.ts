c310f46f473359b002b53108079d16f97af181f5
Renato <renato.melo@ab-inbev.com>
2023-03-20 23:04:46 -0300

    test: fix broken tests

--- src/components/atoms/NavItem/NavItem.test.ts
+++ src/components/atoms/NavItem/NavItem.test.ts
@@ -1,5 +1,7 @@
+import { describe, expect, it } from "vitest"
+
 describe('NavItem', () => {
   it('should be in the document', () => {
-    
+    expect(true).toBe(true)
   })
 })
--- src/components/atoms/Wrapper/Wrapper.test.ts
+++ src/components/atoms/Wrapper/Wrapper.test.ts
@@ -1,5 +1,7 @@
+import { describe, expect, it } from "vitest"
+
 describe('Wrapper', () => {
   it('should be in the document', () => {
-    
+    expect(true).toBe(true)
   })
 })
