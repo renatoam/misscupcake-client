530d802ec022a3b398904f6b423ceca0c66ce983
Renato <renato.melo@ab-inbev.com>
2023-03-21 21:33:17 -0300

    refactor: adjust nav component

--- src/components/atoms/Nav/Nav.tsx
+++ src/components/atoms/Nav/Nav.tsx
@@ -1,14 +1,16 @@
+import { PropsWithChildren } from 'react'
+import Wrapper from '../Wrapper'
 import styles from './Nav.module.scss'
 import { NavProps } from './NavProps'
  
-export default function Nav(props: NavProps) {
+export default function Nav(props: PropsWithChildren<NavProps>) {
   const { className, role, children } = props
   
   return (
-    <nav role={role || 'navigation'}>
-      <ul className={`${styles.ul} ${className}`}>
+    <Wrapper element="nav" role={role || 'navigation'}>
+      <Wrapper element="ul" className={`${styles.ul} ${className}`}>
         {children}
-      </ul>
-    </nav>
+      </Wrapper>
+    </Wrapper>
   )
 }
