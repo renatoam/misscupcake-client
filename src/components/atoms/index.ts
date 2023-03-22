713f4df90bdb0fddbe4827ee8dc6b167fd015cf8
Renato <renato.melo@ab-inbev.com>
2023-03-22 08:34:56 -0300

    chore: create barrel exports to atoms

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,7 @@
+export * as Container from './Container'
+export * as Icon from './Icon'
+export * as Nav from './Nav'
+export * as NavItem from './NavItem'
+export * as Typography from './Typography'
+export * as Wrapper from './Wrapper'
+
