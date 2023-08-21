f66498bb97145a920b5671d777b550d91c403df4
Renato <renato.melo@ab-inbev.com>
2023-08-21 19:32:09 -0300

    chore: remove initial slash from urls

--- src/components/pages/HomePage/HomePage.context.tsx
+++ src/components/pages/HomePage/HomePage.context.tsx
@@ -8,17 +8,17 @@ export const useHomePage = () => useContext(HomePageContext)
 
 export const HomePageContextProvider = ({ children }: any) => {
   const { data: products } = useQuery('homeProducts', async () => {
-    const response = await httpClient.get(`/products?price=lte%3A3&in_stock=eq%3Afalse`)
+    const response = await httpClient.get(`products?price=lte%3A3&in_stock=eq%3Afalse`)
     return response.data
   })
   
   const { data: content } = useQuery('homeContent', async () => {
-    const response = await httpClient.get(`/content/pages?name=home`)
+    const response = await httpClient.get(`content/pages?name=home`)
     return response.data
   })
 
   const mutation = useMutation({
-    mutationFn: async (data) => httpClient.patch(`/content/pages/description`, data)
+    mutationFn: async (data) => httpClient.patch(`content/pages/description`, data)
   })
 
   const value = useMemo(() => ({ products, content, mutation }), [products, content])
